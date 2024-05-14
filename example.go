088da866e9fc255dea85c1cfd562a970f996d89b
Tim Goalen <timgoalen@hotmail.com>
2024-05-14 10:17:12 +0000

    feat: modify json response

--- example.go
+++ example.go
@@ -8,9 +8,9 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "Welcome to my Gin webpage",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
